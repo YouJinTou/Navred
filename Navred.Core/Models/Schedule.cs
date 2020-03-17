6785151243bf13e4453dec07d83383a9be2f4a6d
YouJinTou <dandreevd@gmail.com>
2020-03-17 10:51:18 +0200

    Add project files.

---
+++ Navred.Core/Models/Schedule.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Navred.Core.Models
+{
+    public class Schedule
+    {
+        public string From { get; set; }
+
+        public string To { get; set; }
+
+        public IEnumerable<string> Stops { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
