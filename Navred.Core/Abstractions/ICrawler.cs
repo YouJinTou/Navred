64051cd8c1378a6729f2bc1836e17824436bf7b3
YouJinTou <dandreevd@gmail.com>
2020-03-17 10:57:27 +0200

    Added ICrawler.

---
+++ Navred.Core/Abstractions/ICrawler.cs
@@ -0,0 +1,11 @@
+﻿using Navred.Core.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Navred.Core.Abstractions
+{
+    public interface ICrawler
+    {
+        Task<IEnumerable<Schedule>> GetSchedulesAsync();
+    }
+}
