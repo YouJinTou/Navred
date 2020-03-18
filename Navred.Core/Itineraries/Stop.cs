c98854ecd40722395cfa612a79e3834e86b4e57e
YouJinTou <dandreevd@gmail.com>
2020-03-18 12:03:33 +0200

    Added more validation.

--- Navred.Core/Itineraries/Stop.cs
+++ Navred.Core/Itineraries/Stop.cs
@@ -28,8 +28,21 @@ namespace Navred.Core.Itineraries
                 throw new ArgumentException(
                     $"{nameof(arrivalTime)} must be in the dd:dd format, where 'd' is a digit.");
             }
+
             var hours = int.Parse(arrivalTime.Split(':')[0]);
+
+            if (hours >= 24)
+            {
+                throw new ArgumentException("Hours must be up to 23, inclusive.");
+            }
+
             var minutes = int.Parse(arrivalTime.Split(':')[1]);
+
+            if (minutes >= 60)
+            {
+                throw new ArgumentException("Minutes must be up to 59, inclusive.");
+            }
+
             var hoursTimeSpan = TimeSpan.FromHours(hours);
             var minutesTimeSpan = TimeSpan.FromMinutes(minutes);
             var span = hoursTimeSpan + minutesTimeSpan;
