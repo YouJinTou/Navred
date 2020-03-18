3ffd5e9ffc548f1d49bc6e7ef9cbff543c1350c9
YouJinTou <dandreevd@gmail.com>
2020-03-18 10:48:58 +0200

    Began working on updating itineraries.

---
+++ Navred.Core/Itineraries/Repository.cs
@@ -0,0 +1,36 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Navred.Core.Itineraries
+{
+    public class Repository
+    {
+        private readonly IAmazonDynamoDB db;
+
+        public Repository(IAmazonDynamoDB db)
+        {
+            this.db = db;
+        }
+
+        public async Task UpdateItinerariesAsync(IEnumerable<Itinerary> itineraries)
+        {
+            var updateRequests = itineraries.Select(i => new UpdateItemRequest
+            {
+                TableName = "TN",
+                Key = new Dictionary<string, AttributeValue>
+                {
+                    { "from", new AttributeValue { S = i.From } },
+                    { "datestamp", new AttributeValue { N = i.} }
+                }
+            });
+
+            foreach (var request in updateRequests)
+            {
+                var response = await this.db.UpdateItemAsync(request);
+            }
+        }
+    }
+}
