d74f0d01bc83b7394fad28bd60af081610d6e5dc
YouJinTou <dandreevd@gmail.com>
2020-03-19 16:16:38 +0200

    Added Playground.

---
+++ Navred.Playground/Program.cs
@@ -0,0 +1,14 @@
+﻿using Navred.Core.Cultures;
+
+namespace Navred.Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BulgarianCultureProvider provider = new BulgarianCultureProvider();
+
+            var x = provider.NormalizePlaceName("абланица");
+        }
+    }
+}
