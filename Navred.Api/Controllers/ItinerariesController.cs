8df5b13405a7229dde219618de54d1ae9c300118
YouJinTou <dandreevd@gmail.com>
2020-03-19 10:38:49 +0200

    Implemented ItinerariesController.

---
+++ Navred.Api/Controllers/ItinerariesController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Navred.Core.Itineraries;
+using Navred.Core.Itineraries.DB;
+using System;
+using System.Threading.Tasks;
+
+namespace Navred.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ItinerariesController : Controller
+    {
+        private readonly IItineraryFinder finder;
+        private readonly ILogger<ItinerariesController> logger;
+
+        public ItinerariesController(IItineraryFinder finder, ILogger<ItinerariesController> logger)
+        {
+            this.finder = finder;
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync(
+            string from, string to, DateTime start, DateTime end)
+        {
+            try
+            {
+                var window = new TimeWindow(start, end);
+                var itineraries = await this.finder.FindItinerariesAsync(from, to, window);
+
+                return Ok(itineraries);
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError(ex.ToString());
+
+                return StatusCode(500);
+            }
+        }
+    }
+}
