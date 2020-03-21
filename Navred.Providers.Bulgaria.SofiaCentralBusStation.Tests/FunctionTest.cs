70d55418593f5827c55020cae1a64585f990e3b4
YouJinTou <dandreevd@gmail.com>
2020-03-21 10:24:48 +0200

    Began working on SofiaCentralBusStation.

---
+++ Navred.Providers.Bulgaria.SofiaCentralBusStation.Tests/FunctionTest.cs
@@ -0,0 +1,18 @@
+
+using Amazon.Lambda.TestUtilities;
+using Xunit;
+
+namespace Navred.Providers.SofiaCentralBusStation.Tests
+{
+    public class FunctionTest
+    {
+        [Fact]
+        public void RunsCrawler()
+        {
+
+            var context = new TestLambdaContext();
+            
+            Function.FunctionHandler("hello world", context);
+        }
+    }
+}
---
+++ Navred.Providers.Bulgaria.SofiaCentralBusStation/Function.cs
@@ -0,0 +1,29 @@
+using Amazon.Lambda.Core;
+using Amazon.Lambda.RuntimeSupport;
+using Amazon.Lambda.Serialization.Json;
+using Microsoft.Extensions.DependencyInjection;
+using Navred.Core.Extensions;
+using System;
+using System.Threading.Tasks;
+
+namespace Navred.Providers.SofiaCentralBusStation
+{
+    public class Function
+    {
+        private static async Task Main(string[] args)
+        {
+            Action<string, ILambdaContext> func = FunctionHandler;
+
+            using(var handlerWrapper = HandlerWrapper.GetHandlerWrapper(func, new JsonSerializer()))
+            using(var bootstrap = new LambdaBootstrap(handlerWrapper))
+            {
+                await bootstrap.RunAsync();
+            }
+        }
+
+        public static void FunctionHandler(string input, ILambdaContext context)
+        {
+            var provider = new ServiceCollection().AddCore().BuildServiceProvider();
+        }
+    }
+}
