f0d536cb482985e388f1a679936100ac78553604
YouJinTou <dandreevd@gmail.com>
2020-04-01 00:46:54 +0300

    Added a temporary implementation of the search box.

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './components/search/search.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ui/src/app/components/search/search.component.ts
+++ ui/src/app/components/search/search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
-  constructor() { }
+  searchControl = new FormControl();
+  options: string[] = ['Варна', 'София', 'Търговище', 'Велико Търново', 'Русе', 'Пловдив'];
+  filteredOptions: Observable<string[]>;
 
   ngOnInit() {
+    this.filteredOptions = this.searchControl.valueChanges.pipe(
+      startWith(''),
+      map(v => this.options.filter(o => o.toLowerCase().includes(v.toLowerCase())))
+    );
   }
-
 }
