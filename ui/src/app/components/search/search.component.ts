0b14803d78782132749daf3ed739ffeae8478089
YouJinTou <dandreevd@gmail.com>
2020-04-04 13:12:14 +0300

    We now load places dynamically.

--- ui/src/app/components/search/search.component.ts
+++ ui/src/app/components/search/search.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { placesUrl } from 'src/environments/environment';
 
 @Component({
   selector: 'app-search',
@@ -11,14 +11,16 @@ import { map, startWith } from 'rxjs/operators';
 export class SearchComponent implements OnInit {
   @Output() onPlaceSelected = new EventEmitter<string>();
   searchControl = new FormControl();
-  options: string[] = ['Варна', 'София', 'Търговище', 'Велико Търново', 'Русе', 'Пловдив'];
-  filteredOptions: Observable<string[]>;
+  options: string[] = [];
+
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit() {
-    this.filteredOptions = this.searchControl.valueChanges.pipe(
-      startWith(''),
-      map(v => this.options.filter(o => o.toLowerCase().includes(v.toLowerCase())))
-    );
+    this.searchControl.valueChanges.subscribe(v => {
+      this.httpClient.get<any[]>(placesUrl('Bulgaria', v)).subscribe(places => {
+        this.options = places.map(p => `${p.name} (${p.region}, ${p.municipality})`);
+      });
+    });
   }
 
   onSelect(place: string) {
--- ui/src/environments/environment.sample.ts
+++ ui/src/environments/environment.sample.ts
@@ -5,6 +5,8 @@
 export const environment = {
   production: false
 };
+export const itinerariesUrl = 'URL';
+export const placesUrl = 'URL';
 
 /*
  * For easier debugging in development mode, you can import the following file
