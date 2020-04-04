c270458cdb3b5f1578771e4c7ad5afc37df2cbe2
YouJinTou <dandreevd@gmail.com>
2020-04-04 12:11:23 +0300

    Added PlacesController.

---
+++ src/Navred.Api/Controllers/PlacesController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Navred.Core.Places;
+using Navred.Core.Tools;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Navred.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class PlacesController : Controller
+    {
+        private readonly IPlacesManager placesManager;
+        private readonly ILogger<ItinerariesController> logger;
+
+        public PlacesController(
+            IPlacesManager placesManager, ILogger<ItinerariesController> logger)
+        {
+            this.placesManager = placesManager;
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync(string country, string prefix)
+        {
+            try
+            {
+                Validator.ThrowIfNullOrEmpty(country);
+
+                var places = this.placesManager.LoadPlacesFor<BulgarianPlace>(country);
+                var filteredPlaces = places.Where(
+                    p => p.Name.ToLower().StartsWith(prefix.ToLower()))
+                    .ToList();
+
+                return Ok(await Task.FromResult(filteredPlaces));
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError(ex.ToString());
+
+                return StatusCode(500);
+            }
+        }
+    }
+}
