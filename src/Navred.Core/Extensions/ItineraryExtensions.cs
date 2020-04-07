afa2eb40f74cf3b8592be7f7e739fbc76f4122eb
YouJinTou <dandreevd@gmail.com>
2020-04-08 01:07:48 +0300

    Added an empty check.

--- src/Navred.Core/Extensions/ItineraryExtensions.cs
+++ src/Navred.Core/Extensions/ItineraryExtensions.cs
@@ -7,6 +7,12 @@ namespace Navred.Core.Extensions
         public static string FormatId(this string id)
         {
             var tokens = id.Split('|').Skip(1).ToList();
+
+            if (tokens.IsEmpty())
+            {
+                return id;
+            }
+
             var name = $"{tokens[0]} ({string.Join(", ", tokens.Skip(1))})";
 
             return name;
