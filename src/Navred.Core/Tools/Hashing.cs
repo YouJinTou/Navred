be102bac54511a71b3d1a8e5a6717b0be66be604
YouJinTou <dandreevd@gmail.com>
2020-04-11 20:06:47 +0300

    Added Hashing.

---
+++ src/Navred.Core/Tools/Hashing.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Navred.Core.Tools
+{
+    public static class Hashing
+    {
+        public static string ComputeSha256Hash(string input)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                var bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+                var builder = new StringBuilder();
+
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+
+                return builder.ToString();
+            }
+        }
+    }
+}
