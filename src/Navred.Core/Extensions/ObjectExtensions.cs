d5b20410d31447344ed677cbf87d8faa1ba15767
YouJinTou <dandreevd@gmail.com>
2020-04-17 18:13:49 +0300

    Added ReturnBigger().

--- src/Navred.Core/Extensions/ObjectExtensions.cs
+++ src/Navred.Core/Extensions/ObjectExtensions.cs
@@ -1,10 +1,19 @@
-﻿namespace Navred.Core.Extensions
+﻿using System;
+
+namespace Navred.Core.Extensions
 {
     public static class ObjectExtensions
     {
         public static bool IsNull(this object obj)
         {
             return obj == null;
+        }
+
+        public static T ReturnBigger<T>(this T left, T right) where T : IComparable<T>
+        {
+            var result = left.CompareTo(right);
+
+            return (result > 0) ? left : right;
         }
     }
 }
