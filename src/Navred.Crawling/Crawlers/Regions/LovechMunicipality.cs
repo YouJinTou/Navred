8f923ef513624b45e0b084231285dbcfb581e2cd
YouJinTou <dandreevd@gmail.com>
2020-04-18 14:54:52 +0300

    Finished LovechMunicipality.

--- src/Navred.Crawling/Crawlers/Regions/LovechMunicipality.cs
+++ src/Navred.Crawling/Crawlers/Regions/LovechMunicipality.cs
@@ -11,6 +11,7 @@ using Navred.Core.Processing;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Web;
 using BCP = Navred.Core.Cultures.BulgarianCultureProvider;
@@ -25,16 +26,29 @@ namespace Navred.Crawling.Crawlers
         private readonly ILegRepository repo;
         private readonly ILogger<LovechMunicipality> logger;
         private readonly IDictionary<string, string> regions;
+        private readonly IDictionary<string, string> replacements;
 
         public LovechMunicipality(
-            IRouteParser routeParser, ILegRepository repo, ILogger<LovechMunicipality> logger)
+            IRouteParser routeParser,
+            ILegRepository repo,
+            ILogger<LovechMunicipality> logger)
         {
             this.routeParser = routeParser;
             this.repo = repo;
             this.logger = logger;
             this.regions = new Dictionary<string, string>
             {
-                { "Лисец", BCP.Region.LOV }
+                { "лисец", BCP.Region.LOV },
+                { "скобелево", BCP.Region.LOV },
+                { "брестово", BCP.Region.LOV },
+            };
+            this.replacements = new Dictionary<string, string>
+            {
+                { "Автогара", BCP.City.Lovech },
+                { "с.", string.Empty },
+                { "ЖПС-", "ЖПС" },
+                { "ЖПС", "ЖПС-" },
+                { "Горно Павликени", "Горно Павликене" },
             };
         }
 
@@ -57,76 +71,113 @@ namespace Navred.Crawling.Crawlers
             var web = new HtmlWeb();
             var doc = await web.LoadFromWebAsync(url);
             var rows = doc.DocumentNode
-                .SelectSingleNode("//table").SelectNodes("//tr").Skip(4).ToList();
+                .SelectSingleNode("//table").SelectNodes(".//tr").Skip(4).ToList();
             var all = new List<Leg>();
 
             foreach (var row in rows)
             {
                 var data = row.SelectNodes(".//td").ToList();
-                var names = data[3].InnerText.Replace("Автогара", BCP.Region.LOV).Split('-')
+                var place1 = data[1].InnerText.Split('-').First().Trim().ToLower();
+                var place2 = data[1].InnerText.Split('-').Last().Trim().ToLower();
+                var namesBase = data[3].InnerText
+                    .ChainReplace(this.replacements, false).ToLower().Trim().Split('-');
+                var departureNames = namesBase
+                    .TakeWhileInclusive(s => s.ToLower().Equals(place2))
                     .ToDictionary(kvp => kvp, kvp => this.regions.GetOrDefault(kvp.Trim()));
-                var weekDayTimes = this.GetStopTimes(data[4], data[5], names.Count);
-                var weekendTimes = this.GetStopTimes(data[6], data[7], names.Count);
-                var route = default(Route);
+                var arrivalNames = namesBase
+                    .Reverse()
+                    .SkipUntilLast(place2)
+                    .ToDictionary(kvp => kvp, kvp => this.regions.GetOrDefault(kvp.Trim()));
+                var weekDayDepartures = this.GetStopTimes(data[4], departureNames.Count);
+                var weekDayArrivals = this.GetStopTimes(data[5], arrivalNames.Count);
+                var weekendDepartures = this.GetStopTimes(data[6], departureNames.Count);
+                var weekendArrivals = this.GetStopTimes(data[7], arrivalNames.Count);
+                var legs = new List<Leg>();
+                var weekDayDepartureLegs = await this.GetLegsAsync(
+                    weekDayDepartures, departureNames, Constants.MondayToFriday);
+                var weekDayArrivalLegs = await this.GetLegsAsync(
+                    weekDayArrivals, arrivalNames, Constants.MondayToFriday);
+                var weekendDepartureLegs = await this.GetLegsAsync(
+                    weekendDepartures, departureNames, Constants.Weekend | DaysOfWeek.HolidayInclusive);
+                var weekendArrivalLegs = await this.GetLegsAsync(
+                    weekendArrivals, arrivalNames, Constants.Weekend | DaysOfWeek.HolidayInclusive);
 
+                legs.AddRange(weekDayDepartureLegs);
+                legs.AddRange(weekDayArrivalLegs);
+                legs.AddRange(weekendDepartureLegs);
+                legs.AddRange(weekendArrivalLegs);
+                all.AddRange(legs);
+            }
+
+            return all;
+        }
+
+        private IList<IEnumerable<string>> GetStopTimes(HtmlNode node, int count)
+        {
+            var times = node
+                .SelectNodes(".//p")
+                .Where(n => !string.IsNullOrWhiteSpace(HttpUtility.HtmlDecode(n.InnerText)))
+                .Select(n => new[]
+                {
+                    HttpUtility.HtmlDecode(n.InnerText),
+                    null
+                }.InsertBetween(null, count))
+                .ToList();
+
+            return times;
+        }
+
+        private async Task<IEnumerable<Leg>> GetLegsAsync(
+            IList<IEnumerable<string>> timesList,
+            IDictionary<string, string> names,
+            DaysOfWeek dow)
+        {
+            var all = new List<Leg>();
+            var schedule = string.Join(" ", timesList.SelectMany(l => l.Select(s => s)));
+            var dows = Regex.Matches(schedule, @"(\d{1,2}[.,:]\d{1,2})([\D]+)");
+
+            for (int t = 0, m = 0; t < timesList.Count; t++, m++)
+            {
                 try
                 {
-                    foreach (var times in weekDayTimes)
-                    {
-                        var stops = Stop.CreateMany(names.Keys, times, regions: names.Values);
-                        route = new Route(
-                            BulgarianCultureProvider.CountryName,
-                            Constants.MondayToFriday,
-                            "Община Ловеч",
-                            Mode.Bus,
-                            stops,
-                            Url);
-                        var legs = await this.routeParser.ParseRouteAsync(route);
+                    var match = dows[m];
+                    var stops = Stop.CreateMany(names.Keys, timesList[t], regions: names.Values);
+                    var route = new Route(
+                        BulgarianCultureProvider.CountryName,
+                        this.GetDow(match?.Groups[2]?.Value, dow),
+                        "Община Ловеч",
+                        Mode.Bus,
+                        stops,
+                        Url);
+                    var legs = await this.routeParser.ParseRouteAsync(route);
+                    t = string.IsNullOrWhiteSpace(match?.Groups[2]?.Value) ? t : t + 1;
 
-                        all.AddRange(legs);
-                    }
+                    all.AddRange(legs);
                 }
                 catch (Exception ex)
                 {
-                    this.logger.LogError(ex, "Row failed.");
-                }
-
-                try
-                {
-                    foreach (var times in weekendTimes)
-                    {
-                        var stops = Stop.CreateMany(names.Keys, times, regions: names.Values);
-                        route = route.Copy(Constants.Weekend, stops);
-                        var legs = await this.routeParser.ParseRouteAsync(route);
-
-                        all.AddRange(legs);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    this.logger.LogError(ex, "Row failed.");
+                    this.logger.LogError(
+                        ex, $"{string.Join(" ", names)} | {string.Join(" ", timesList[t])}");
                 }
             }
 
             return all;
         }
 
-        private IList<IEnumerable<string>> GetStopTimes(HtmlNode first, HtmlNode second, int count)
+        private DaysOfWeek GetDow(string s, DaysOfWeek current)
         {
-            var times = first
-                .SelectNodes(".//p")
-                .Where(n => !string.IsNullOrWhiteSpace(HttpUtility.HtmlDecode(n.InnerText)))
-                .Zip(second
-                    .SelectNodes(".//p")
-                    .Where(n => !string.IsNullOrWhiteSpace(HttpUtility.HtmlDecode(n.InnerText))))
-                .Select(fs => new[]
-                {
-                    HttpUtility.HtmlDecode(fs.First.InnerText),
-                    HttpUtility.HtmlDecode(fs.Second.InnerText)
-                }.InsertBetween(null, count))
-                .ToList();
+            var result = Regex.Replace(s.Trim(), $@"[^{BCP.AllLetters}]", string.Empty);
 
-            return times;
+            return result switch
+            {
+                "с" => DaysOfWeek.Saturday,
+                "н" => DaysOfWeek.Sunday,
+                "ср" => DaysOfWeek.Wednesday,
+                "всряда" => DaysOfWeek.Wednesday,
+                "всрядаипетък" => DaysOfWeek.Wednesday | DaysOfWeek.Friday,
+                "внед" => DaysOfWeek.Sunday,
+                _ => current
+            };
         }
     }
 }
