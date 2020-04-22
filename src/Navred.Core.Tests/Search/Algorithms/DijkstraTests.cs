719e5b02de4b7ae1f1bce7cb29931d0d25cf4333
YouJinTou <dandreevd@gmail.com>
2020-04-22 23:14:42 +0300

    Began working on DijkstraTests.

---
+++ src/Navred.Core.Tests/Search/Algorithms/DijkstraTests.cs
@@ -0,0 +1,25 @@
+﻿using Navred.Core.Search.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Navred.Core.Tests.Search.Algorithms
+{
+    public class DijkstraTests
+    {
+        private Dijkstra Dijkstra => new Dijkstra();
+
+        [Fact]
+        public void GraphNull_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() => this.Dijkstra.FindKShortestPaths(null, 0));
+        }
+
+        [Fact]
+        public void NoConnectionEdges_ReturnsZeroPaths()
+        {
+
+        }
+    }
+}
---
+++ src/Navred.Core.Tests/Search/GraphBuilder.cs
@@ -0,0 +1,86 @@
+﻿using Navred.Core.Extensions;
+using Navred.Core.Search;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Navred.Core.Tests.Search
+{
+    public class GraphBuilder
+    {
+        private readonly Random rand;
+        private Vertex source;
+        private Vertex destination;
+        private IEnumerable<Vertex> vertices;
+        private IEnumerable<Edge> edges;
+
+        public GraphBuilder()
+        {
+            this.rand = new Random();
+        }
+
+        public GraphBuilder BuildGraph()
+        {
+            return this;
+        }
+
+        public GraphBuilder WithVertices(int n)
+        {
+            if (n <= 1)
+            {
+                throw new ArgumentException("Number of vertices must be greater than two.");
+            }
+
+            var vertices = new List<Vertex>();
+
+            for (int i = 0; i < n; i++)
+            {
+                vertices.Add(new Vertex
+                {
+                    Name = this.rand.NextDouble().ToString()
+                });
+            }
+
+            this.source = vertices.First();
+            this.destination = vertices.Last();
+            this.vertices = vertices;
+
+            return this;
+        }
+
+        public GraphBuilder WithEdges(int n, int paths)
+        {
+            if (this.vertices.IsNullOrEmpty())
+            {
+                throw new InvalidOperationException("Cannot add edges before vertices.");
+            }
+
+            if (paths >= n)
+            {
+                throw new InvalidOperationException("Paths more than edges.");
+            }
+
+            var edges = new List<Edge>();
+
+            for (int i = 0; i < n; i++)
+            {
+                edges.Add(new Edge
+                {
+                    //Source = this.source,
+                    //Destination = this.destination
+                });
+            }
+
+            this.edges = edges;
+
+            return this;
+        }
+
+        public Graph Build()
+        {
+            var graph = new Graph(this.source, this.destination, this.vertices, this.edges);
+
+            return graph;
+        }
+    }
+}
